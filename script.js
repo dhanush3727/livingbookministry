eb2227fe3fbefac7f18d65d0ee3b121416083adc
dhanush3727 <dhanushs4827@gmail.com>
2023-11-25 22:28:46 +0530

    Updated

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
-// NavBar
-const hamBurgerMenu = document.querySelector(".menu");
-const searchItem = document.querySelector(".search-item");
+// Large Screen NavBar
 const listItems = document.querySelector(".list-items");
+const searchBar = document.querySelector(".search-bar");
+const searchImg = document.querySelector(".search");
+const closeImg = document.querySelector(".close");
 
-hamBurgerMenu.addEventListener("click", () => {
-  searchItem.classList.toggle("show");
-  listItems.classList.toggle("show");
+searchImg.addEventListener("click", () => {
+  searchImg.style.display = "none";
+  listItems.style.display = "none";
+  searchBar.style.display = "block";
+  closeImg.style.display = "block";
+});
+closeImg.addEventListener("click", () => {
+  searchImg.style.display = "block";
+  listItems.style.display = "flex";
+  searchBar.style.display = "none";
+  closeImg.style.display = "none";
+});
+
+// Mobile Navbar
+const mobile = document.querySelector(".mobile");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuIcon = document.querySelector(".menu");
+const closeIcon = document.querySelector(".closeMenu");
+const mobileSearch = document.querySelector(".mobile-search");
+const mobileClose = document.querySelector(".mobile-close");
+const mobileSearchBar = document.querySelector(".mobile-search-bar");
+
+menuIcon.addEventListener("click", () => {
+  menuIcon.style.display = "none";
+  closeIcon.style.display = "block";
+  mobileMenu.style.display = "block";
+});
+closeIcon.addEventListener("click", () => {
+  menuIcon.style.display = "block";
+  closeIcon.style.display = "none";
+  mobileMenu.style.display = "none";
+});
+mobileSearch.addEventListener("click", () => {
+  mobileSearch.style.display = "none";
+  mobileClose.style.display = "block";
+  mobileSearchBar.style.display = "block";
+});
+mobileClose.addEventListener("click", () => {
+  mobileSearch.style.display = "block";
+  mobileClose.style.display = "none";
+  mobileSearchBar.style.display = "none";
 });
 
 // Search Book
@@ -29,4 +68,29 @@ document.addEventListener("click", (event) => {
     keywordImg.classList.remove("active");
     keywordList.classList.remove("showList");
   }
+});
+
+// Image Scroll
+const leftArrow = document.querySelector(".left-arr");
+const rightArrow = document.querySelector(".right-arr");
+const iconOne = document.querySelector(".stream-icon:nth-child(1)");
+const cardOne = document.querySelector(".cardOne");
+const left = document.querySelector(".arrow-img:nth-child(1)");
+const right = document.querySelector(".arrow-img:nth-child(2)");
+
+rightArrow.addEventListener("click", () => {
+  iconOne.style.margin = "0 0 0 -198px";
+  rightArrow.style.visibility = "hidden";
+  leftArrow.style.visibility = "visible";
+});
+leftArrow.addEventListener("click", () => {
+  iconOne.style.margin = "0 0 0 0";
+  rightArrow.style.visibility = "visible";
+  leftArrow.style.visibility = "hidden";
+});
+left.addEventListener("click", () => {
+  cardOne.style.margin = "0 0 0 0";
+});
+right.addEventListener("click", () => {
+  cardOne.style.margin = "0 0 0 -330px";
 });
